7cb161cdf99fb1761a2d95688ec02dcf8542770f
shaopeng wu <shaopeng.wu@ims-evolve.com>
2026-01-05 14:14:20 +0000

    update environmental configuration;

--- app/query/route.ts
+++ app/query/route.ts
@@ -2,6 +2,7 @@ import postgres from 'postgres';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
+
 async function listInvoices() {
 	const data = await sql`
     SELECT invoices.amount, customers.name
